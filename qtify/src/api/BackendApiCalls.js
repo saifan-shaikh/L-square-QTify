1e92123f84ff677b02c4c4f32c28c7971a756675
saifan-shaikh <saifanshk97@gmail.com>
2023-03-14 22:29:19 +0530

    cardCarouselNewAlbums-qtify-takehome

--- qtify/src/api/BackendApiCalls.js
+++ qtify/src/api/BackendApiCalls.js
@@ -2,9 +2,21 @@ import axios from "axios";
 import { BACKEND_URL } from "./api";
 
 // Top Albums
-export const fetchTopSongs = async () => {
+export const fetchTopAlbums = async () => {
   try {
     const res = await axios.get(`${BACKEND_URL}/albums/top`);
+    const data = await res.data;
+    // console.log("Top Album: ", data);
+    return data;
+  } catch (err) {
+    return { status: err.response.status, message: err.message };
+  }
+};
+
+//New Albums
+export const fetchNewAlbums = async () => {
+  try {
+    const res = await axios.get(`${BACKEND_URL}/albums/new`);
     const data = await res.data;
     // console.log("Top Album: ", data);
     return data;
--- qtify/src/components/HomePage/HomePage.js
+++ qtify/src/components/HomePage/HomePage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { fetchTopSongs } from "../../api/BackendApiCalls";
+import { fetchNewAlbums, fetchTopAlbums } from "../../api/BackendApiCalls";
 import Card from "../Card/Card";
 import Header from "../Header/Header";
 import HeroBackground from "../HeroBackground/HeroBackground";
@@ -15,9 +15,10 @@ const HomePage = (props) => {
 
       {/* Body */}
       <div style={{ margin: "0 1rem" }}>
-        {/* Card */}
-        <Section sectionTitle={"Top Albums"} dataSource={fetchTopSongs} />
-        <Card />
+        {/* Top Albums */}
+        <Section sectionTitle={"Top Albums"} dataSource={fetchTopAlbums} />
+        {/* New Albums */}
+        <Section sectionTitle={"New Albums"} dataSource={fetchNewAlbums} />
       </div>
     </div>
   );
